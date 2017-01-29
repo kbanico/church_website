09cfc284275870145e57d6354624acd790ab205a
ken <code4uguam@gmail.com>
2017-01-29 02:54:18 +0000

    calender showing and outputing date from controller

--- app/controllers/prayers_controller.rb
+++ app/controllers/prayers_controller.rb
@@ -1,6 +1,25 @@
 class PrayersController < ApplicationController
     def index
         @prayers = Prayer.all
+        #@prayers_by_date = @prayers.group(&:created_at.strftime('%Y-%m-%d'))
+      
+        
+        @day = Date.today.strftime("%Y-%m-%d")
+        
+        # FOR INDEX PAGE
+        # <%= calendar do |date| %>
+        #     <%= date.day %>
+        #     <% if @prayers_by_date[date] %>
+        #         <ul>
+        #             <% @prayers_by_date[date].each do |prayer| %>
+        #                 <li><%= link_to prayer.name, "#" %></li>
+        #             <% end %>
+        #         </ul>
+        #     <% end %>
+        # <% end %>
+            
+
+        
     end
     
     def new
